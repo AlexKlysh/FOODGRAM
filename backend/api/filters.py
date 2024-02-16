3bbffd469ba32847bd2096e684a2c7a957fd79a9
AlexKlysh <138516713+AlexKlysh@users.noreply.github.com>
2024-02-16 09:46:47 +0300

    Update filters.py

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -12,8 +12,8 @@ class RecipeFilter(django_filters.FilterSet):
         queryset=Tag.objects.all()
     )
 
-    is_favorited = django_filters.BooleanFilter(method='filter_is_favorited')
-    is_in_shopping_cart = django_filters.BooleanFilter(
+    is_favorited = django_filters.NumberFilter(method='filter_is_favorited')
+    is_in_shopping_cart = django_filters.NumberFilter(
         method='filter_is_in_shopping_cart'
     )
 
